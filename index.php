bbce95091ba4605f8900c0d743ffed4445c53b18
Navika-M <47578845+Navika-M@users.noreply.github.com>
2021-03-29 23:31:07 -0600

    things

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skills-Shopping</title>
+
+    <!-- Bootstrap Links -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Zilla+Slab:ital,wght@0,300;1,300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Clothing Retailer -->
+    <!-- Navigation Bar -->
+
+    <nav class="navbar navbar-expand-sm bg-light">
+        <div class="container-fluid">
+
+            <a class="navbar-header text-dark display-4 px-5" href="#">Clothing Retailer</a>
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link px-5" href="#">About Us</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-5" href="#">Customer Support</a>
+                </li>
+            </ul>
+        </div>
+
+
+    </nav>
+
+
+    
+</body>
+
+</html>
